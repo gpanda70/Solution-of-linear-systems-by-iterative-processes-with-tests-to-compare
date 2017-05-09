ec15230e627f8bad6f13ca9dfeac1dc0b0e835a3
James Im <gpanda@uga.edu>
2017-05-09 15:50:38 -0400

    Update compare.m

--- compare.m
+++ compare.m
@@ -1,13 +1,22 @@
 function [ ] = compare( A,b,x,n )
-%compares the 3 different methods
+%compares the 3 different methods.
+%A is 50x50 matrix that is positive definite, symmetric and diagonally
+%dominant.
+%b and x are vectors where Ax=b where we are solving for x so we set x as
+%x=zeros(50,1) to keep it empty while we fill it for values in the gs,js
+%and steepD methods.
+%n is number of iterations.
+
+%A=rand(50);
+%A=A'*A + 1000*eye(50);
+%b=rand(50,1);
+%x=zeros(50,1);
+
 fprintf( 'Running all 3 methods with %d iterations stopping at error 1e-5\n', n )
 fprintf(['They will be tested with a random 50x50 matrix that' ...
       'is positive definite, symmetric and diagonally dominant\n\n'])
   
- % A=rand(100);
-  %A=A'*A + 1000*eye(100);
-  %b=rand(100,1);
-  %x=zeros(100,1);
+
   
   A1=gs(A,b,n,x,1e-5);
   normg=norm(A*A1-b,Inf);
@@ -23,4 +32,3 @@ fprintf(['They will be tested with a random 50x50 matrix that' ...
   
 
 end
-
